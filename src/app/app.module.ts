e476f5d2d04b4cb4fb99b042178110d6f7c57c73
Nadezhda Avramovska <saskomkd33@gmail.com>
2020-09-04 00:48:07 +0200

    update commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatchesComponent } from './matches/matches.component';
+import { MatchDetailsComponent } from './match-details/match-details.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { CompareTeamsComponent } from './compare-teams/compare-teams.component';
+import { MyMatchesComponent } from './my-matches/my-matches.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatchManagementService} from './match-management/match-management.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchesComponent,
+    MatchDetailsComponent,
+    CompareTeamsComponent,
+    MyMatchesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+
+  
+  providers: [MatchManagementService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
